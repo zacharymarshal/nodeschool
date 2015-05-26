a02fe11786622f4d63508e4175dda58f7cf3d39f
Zachary Rankin <rankin.zachary@gmail.com>
2015-05-25 21:44:27 -0700

    trying out nodeschool

---
+++ learnyournode/2_baby_steps.js
@@ -0,0 +1,17 @@
+// My Solution
+console.log(
+  process.argv.slice(2).reduce(
+    function(previousValue, currentValue) {
+      return Number(previousValue) + Number(currentValue);
+    },
+    0
+  )
+);
+
+// Their Solution
+// var result = 0
+
+// for (var i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+
+// console.log(result)
