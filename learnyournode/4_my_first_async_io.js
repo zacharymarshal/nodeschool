c7f59e061d16a808f36ecb7ecc10990a4023a252
Zachary Rankin <rankin.zachary@gmail.com>
2015-05-26 09:40:19 -0700

    Implement async io
    Make it pass

--- learnyournode/4_my_first_async_io.js
+++ learnyournode/4_my_first_async_io.js
@@ -1,9 +1,21 @@
+'use strict';
+
 var fs = require('fs');
 var filepath = process.argv[2];
 
-fs.readFile(filepath, function(err, data) {
+fs.readFile(filepath, 'utf-8', function(err, data) {
   if (err) {
     throw err;
   }
-  console.log(data);
+  console.log(data.split('\n').length - 1);
 });
+
+// Their Solution
+// var fs = require('fs')
+// var file = process.argv[2]
+
+// fs.readFile(file, function (err, contents) {
+//   // fs.readFile(file, 'utf8', callback) can also be used
+//   var lines = contents.toString().split('\n').length - 1
+//   console.log(lines)
+// })
