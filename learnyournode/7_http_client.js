2896da832064ae7539597119c39bcf2da50fa9f8
Zachary Rankin <rankin.zachary@gmail.com>
2015-05-27 11:53:28 -0700

    Update output handling
    I like their solution to outputting the data and error logs.

--- learnyournode/7_http_client.js
+++ learnyournode/7_http_client.js
@@ -6,9 +6,8 @@ var url = process.argv[2];
 
 http.get(url, function(res) {
   res.setEncoding('utf8');
-  res.on('data', function(data) {
-    console.log(data);
-  });
+  res.on('data', console.log);
+  res.on('error', console.error);
 });
 
 // Their Solution
