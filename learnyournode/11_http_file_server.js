348191acbb5d0d822dcc6eba2b0ac68b0d209056
Zachary Rankin <rankin.zachary@gmail.com>
2015-05-27 21:07:10 -0700

    Update based on nodeschool solution
    - Add a status code of 200 and a Content-Type header
    - Convert the port to a number

--- learnyournode/11_http_file_server.js
+++ learnyournode/11_http_file_server.js
@@ -7,9 +7,11 @@ var port = process.argv[2];
 var fileToServe = process.argv[3];
 
 var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
   fs.createReadStream(fileToServe).pipe(res);
 });
-server.listen(port);
+server.listen(Number(port));
 
 // Their Solution
 // var http = require('http')
