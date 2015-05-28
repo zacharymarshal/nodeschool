15b014650d0b60190a7759f32d5af8434e70cc05
Zachary Rankin <rankin.zachary@gmail.com>
2015-05-27 20:41:29 -0700

    Implement tcp time server

---
+++ learnyournode/10_time_server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var net = require('net');
+var strftime = require('strftime');
+
+var port = process.argv[2];
+
+var server = net.createServer(function(socket) {
+  socket.end(strftime('%F %H:%M%n'));
+});
+server.listen(port);
+
+// Their solution
+// var net = require('net')
+
+// function zeroFill(i) {
+//   return (i < 10 ? '0' : '') + i
+// }
+
+// function now () {
+//   var d = new Date()
+//   return d.getFullYear() + '-'
+//     + zeroFill(d.getMonth() + 1) + '-'
+//     + zeroFill(d.getDate()) + ' '
+//     + zeroFill(d.getHours()) + ':'
+//     + zeroFill(d.getMinutes())
+// }
+
+// var server = net.createServer(function (socket) {
+//   socket.end(now() + '\n')
+// })
+
+// server.listen(Number(process.argv[2]))
