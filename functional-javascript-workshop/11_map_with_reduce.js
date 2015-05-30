4c29c7192c60a98ab5e886907f5034965f906c42
Zachary Rankin <rankin.zachary@gmail.com>
2015-05-29 21:19:36 -0700

    Allow thisArg to be passed
    I noticed in the solution that they supported passing a thisArg
    argument, for determining the usage of “this” within the map (which is
    what a normal map allows for).

--- functional-javascript-workshop/11_map_with_reduce.js
+++ functional-javascript-workshop/11_map_with_reduce.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = function(arr, fn) {
-  return arr.reduce(function(newArr, item) {
-    newArr.push(fn(item));
+module.exports = function(arr, fn, thisArg) {
+  return arr.reduce(function(newArr, item, index, arr) {
+    newArr.push(fn.call(thisArg, item, index, arr));
 
     return newArr;
   }, []);
