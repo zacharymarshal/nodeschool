1b54b019e6d5be228346899881cbcff1d87ed725
Zachary Rankin <rankin.zachary@gmail.com>
2015-06-01 15:08:56 -0700

    Implement currying lesson

---
+++ functional-javascript-workshop/17_currying.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function curryN(fn, n) {
+  n = n || fn.length;
+
+  return function curry(arg) {
+    if (n === 1) {
+      return fn(arg);
+    }
+
+    return curryN(fn.bind(this, arg), n - 1);
+  };
+}
+
+module.exports = curryN;
