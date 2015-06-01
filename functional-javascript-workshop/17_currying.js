601ec4aebde51b560113519d7ba2577e632a868e
Zachary Rankin <rankin.zachary@gmail.com>
2015-06-01 15:14:25 -0700

    Implement dynamic number of args
    Spicing things up a bit to support a dynamic number of arguments for fun

--- functional-javascript-workshop/17_currying.js
+++ functional-javascript-workshop/17_currying.js
@@ -3,13 +3,22 @@
 function curryN(fn, n) {
   n = n || fn.length;
 
-  return function curry(arg) {
-    if (n === 1) {
-      return fn(arg);
+  return function curry() {
+    var args = Array.prototype.slice.call(arguments);
+    if (args.length === n) {
+      return fn.apply(this, args);
     }
 
-    return curryN(fn.bind(this, arg), n - 1);
+    return curryN(fn.bind.apply(fn, [this].concat(args)), n - args.length);
   };
 }
+
+function add3(a, b, c) {
+  return a + b + c;
+}
+
+console.log(curryN(add3)(1)(2)(3) === 6);
+console.log(curryN(add3)(1, 2)(3) === 6);
+console.log(curryN(add3)(1, 2, 3) === 6);
 
 module.exports = curryN;
